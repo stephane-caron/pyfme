1aa750caeb69c3d1a0481de853c9935678def5ad
Stéphane Caron <stephane.caron@normalesup.org>
2017-11-12 13:43:55 +0100

    License changed in setup.py as well

--- setup.py
+++ setup.py
@@ -6,16 +6,17 @@
 # This file is part of PyFME <https://github.com/stephane-caron/PyFME>.
 #
 # PyFME is free software: you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
 #
 # PyFME is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
 #
-# You should have received a copy of the GNU General Public License along with
-# PyFME. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License along
+# with PyFME. If not, see <http://www.gnu.org/licenses/>.
 
 from distutils.core import setup
 
@@ -32,7 +33,7 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Mathematics',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: GNU Lesser General Public License (LGPL)',
         'Programming Language :: Python :: 2.7'],
     packages=['pyfme']
 )
